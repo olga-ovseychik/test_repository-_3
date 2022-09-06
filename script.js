9b5e581448ffc5d01e4c52e62962cb2cf246cb52
Olga Morozova <olga.ovsejchik@gmail.com>
2022-09-06 23:46:23 +0300

    Added task-2

--- script.js
+++ script.js
@@ -3,3 +3,13 @@
 //--Task-1--//
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели');
+
+//--Task-2--//
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
