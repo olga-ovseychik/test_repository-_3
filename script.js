889b46704be98a439561a1948c840e207ba0abdc
Olga Morozova <olga.ovsejchik@gmail.com>
2022-09-08 22:36:53 +0300

    Modified task-3 and added implementation of task-3 in two other ways

--- script.js
+++ script.js
@@ -14,13 +14,59 @@ const personalMovieDB = {
     privat: false
 };
 
-//--Task-3--//
+//--modofied task-3--//
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько оцените его', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {  
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
+
+//-- task-3 (while)--//
+
+let i = 0;
+while (i < 2) {
+    i++;
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {  
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+//-- task-3 (do-while)--//
+
+do {
+    i++;
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько оцените его', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {  
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+} while (i < 2);
 
 console.log(personalMovieDB);
+
+
 
